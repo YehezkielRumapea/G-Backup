927f93a6908dd2a2bc54ec404a4656b5f1f5b457
idvenmanurung <jrmanurung2@gmail.com>
2025-11-04 14:13:10 +0700

    new

--- backend/internal/service/service_utility.go
+++
@@ -1,7 +0,0 @@
-package service
-
-import "sync"
-
-// LogMutex adalah Mutex (Lock) yang melindungi akses ke LogRepository
-// saat Goroutine (Job) menulis ke database secara bersamaan.
-var LogMutex sync.Mutex
